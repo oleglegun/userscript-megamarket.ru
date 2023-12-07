77a66cb1c22029cd568adcb00ac184ac8f6324f4
Oleg Legun <oleg.legun@gmail.com>
2023-12-07 14:29:29 +0300

    update script meta

---
+++ script.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         [megamarket.ru] Show adjusted prices
+// @namespace    http://tampermonkey.net/
+// @version      1.0.3
+// @description  Sorry. Nothing interesting here.
+// @author       Persona
+// @match        https://megamarket.ru/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=megamarket.ru
+// @grant        none
+// ==/UserScript==
+
